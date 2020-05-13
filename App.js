e62086673c067dba7f3adb37c1d44fac909b745f
josh-web <joshuamrahi@gmail.com>
2020-05-13 19:53:11 +0530

    Weather Forecast

--- App.js
+++ App.js
@@ -1,19 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class WeatherScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weather: '',
+    };
+  }
+
+  getWeather = async () => {
+    //change latitude and longitude
+    var url = 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139';
+    return fetch(url)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          weather: responseJson,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  componentDidMount = () => {
+    this.getWeather();
+  };
+
+  render() {
+    if (this.state.weather === '') {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.subContainer}>
+            <Text style={styles.title}>
+              Weather Forecast
+            </Text>
+            <Image
+              style={styles.cloudImage}
+              source={require('./weatherImg.png')}
+            />
+            <View style={styles.textContainer}>
+            <Text style={{ fontSize: 18}}>
+              {this.state.weather.main.temp}&deg;C
+            </Text>
+            <Text style={{ fontSize: 20, margin:10}}>
+              humidity : {this.state.weather.main.humidity}
+            </Text>
+            <Text style={{fontSize: 20}}>
+              {this.state.weather.weather[0].description}
+            </Text>
+          </View>
+          </View>    
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex:1
   },
+  subContainer : { 
+    flex: 1, 
+    borderWidth: 1, 
+    alignItems: 'center' 
+    },
+    title:{ 
+      marginTop: 50, 
+      fontSize: 30,
+    },
+    cloudImage :{ 
+      width: 200, 
+      height: 200, 
+      marginTop: 30 
+    },
+    textContainer : { 
+      flex: 1,
+      alignItems: 'center', 
+      flexDirection:'row', 
+      marginTop:-150
+    }
 });
+
